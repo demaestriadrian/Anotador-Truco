ced117e35b1825ca9533dbe62f5afabf2903ea60
adrian-demaestri-work <126787165+adrian-demaestri-work@users.noreply.github.com>
2023-04-27 00:22:32 -0300

    cambios imcompletos fosforos height

---
+++ javascript/fosforo.class.js
@@ -0,0 +1,9 @@
+const containerFosforo = document.querySelector('.containerFosforo')
+const reduceHeight = .75
+export default class Fosforo {
+    static height = containerFosforo.clientHeight * reduceHeight;
+
+    static setHeight() {
+        
+    }
+}
