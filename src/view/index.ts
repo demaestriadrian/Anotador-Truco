13f762a6e4a3648a1671d9fb8abe58b5eeba4ecf
adrian-demaestri-work <126787165+adrian-demaestri-work@users.noreply.github.com>
2023-10-30 22:21:38 -0300

    configurando less y linter

--- src/view/index.ts
+++ src/view/index.ts
@@ -1,7 +1,7 @@
-import './less/index.less'
-// import { sections } from "./elements.ts";
-import { sizeMatchstickStorage } from "./sizes.ts";
 
-export const initView = ()=>{
-  sizeMatchstickStorage()
+// import { sections } from "./elements.ts";
+import { sizeMatchstickStorage } from "./sizes";
+
+export const initView = () => {
+      sizeMatchstickStorage()
 }
