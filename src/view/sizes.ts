a5452f08f5a9ec08d120658a6bc663a32bb5c2c1
demaestriadrian <99728381+demaestriadrian@users.noreply.github.com>
2023-11-16 23:33:09 -0300

    add styles

--- src/view/sizes.ts
+++ src/view/sizes.ts
@@ -4,21 +4,15 @@ const sizeMatchstickStorage = (): void => {
   const mobileOS = ['ios', 'android']
   const userAgent = navigator.userAgent.toLowerCase()
 
-  if (mobileOS.some(os => userAgent.includes(os))) {
-    matchstickStorage.style.flex = '5'
-    window.alert(matchstickStorage)
-  }
+  if (mobileOS.some(os => userAgent.includes(os))) { matchstickStorage.style.flex = '5' }
 }
 
 const sizeMatchstick = (): void => {
-  console.log(matchstickPositionElement.clientHeight + 'px')
   Object.defineProperty(window, 'matchstickPositionElement', { value: matchstickPositionElement })
   allMatchsticks.forEach(matchstick => (matchstick.style.height = matchstickPositionElement.clientHeight + 'px'))
 }
 
 const initSizes = (): void => {
-  console.log('initSizes')
-
   sizeMatchstickStorage()
   sizeMatchstick()
 }
